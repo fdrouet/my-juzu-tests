201e00eba1e5ef91decd4c8ba678f5e10b3dd9c8
Frederic DROUET <fdrouet@exoplatform.com>
2013-08-19 19:51:43 +0200

    Add AngularJS with juju WebJars plugin

--- src/main/java/org/juzu/mytests/package-info.java
+++ src/main/java/org/juzu/mytests/package-info.java
@@ -1,9 +1,12 @@
 @Application(defaultController = MyApplication.class)
+@WebJars("angular.js")
 @Assets (scripts = {
-    @Script( src = "my.js")
+    @Script( src = "my.js"),
+    @Script( id = "angular", src = "angular.js")
 })
 package org.juzu.mytests;
 
 import juzu.Application;
 import juzu.plugin.asset.Assets;
 import juzu.plugin.asset.Script;
+import juzu.plugin.webjars.WebJars;
