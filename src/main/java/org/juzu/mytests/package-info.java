4cfff21903764e92190834c0d624aa32467f3467
Frederic DROUET <fdrouet@exoplatform.com>
2014-05-07 21:49:17 +0200

    Upgrade to Bootstrap 3.1.1

--- src/main/java/org/juzu/mytests/package-info.java
+++ src/main/java/org/juzu/mytests/package-info.java
@@ -3,14 +3,15 @@
 @Scripts({
     @Script( value = "my.js"),
     @Script( id="angular.js", value= "//ajax.googleapis.com/ajax/libs/angularjs/1.1.5/angular.min.js", location = AssetLocation.URL),
-    @Script( id="bootstrap.js", value = "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js", location = AssetLocation.URL)
+    @Script( id="bootstrap.js", value = "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js", location = AssetLocation.URL)
 })
 @Stylesheets({
     @Stylesheet(value = "my.css"),
-    @Stylesheet(id="bootstrap.css", value = "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css", location = AssetLocation.URL)
+    @Stylesheet(id="bootstrap.css", value = "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css", location = AssetLocation.URL),
+    @Stylesheet(id="bootstrap-theme.css", value = "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css", location = AssetLocation.URL)
 })
 
-@Assets({"angular.js", "bootstrap.js", "bootstrap.css", "my.js", "my.css"})
+@Assets("*")
 package org.juzu.mytests;
 
 import juzu.Application;
